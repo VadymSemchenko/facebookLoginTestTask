b2e89200ecfbcf46addc2d4a964314c3f2725414
vsemchenko-mindk <vsemchenko@mindk.com>
2019-08-26 23:47:23 +0300

    app logs in with facebook sdk

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, Button} from 'react-native';
+import {LoginButton, AccessToken} from 'react-native-fbsdk';
 
 import {Screen} from '../components';
 import {ROUTENAMES} from '../constants';
@@ -11,6 +12,20 @@ const Login = ({navigation}) => {
   return (
     <Screen>
       <Button onPress={proceedToProfile} title="to profile" />
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log('login has error: ' + result.error);
+          } else if (result.isCancelled) {
+            console.log('login is cancelled.');
+          } else {
+            AccessToken.getCurrentAccessToken().then(data => {
+              console.log(data);
+            });
+          }
+        }}
+        onLogoutFinished={() => console.log('logout.')}
+      />
       <Text style={{color: 'white'}}>login</Text>
     </Screen>
   );
